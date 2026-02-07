75d827586c75713052cc2878165f6f856e91e022
33bca <fabian.leutenegger@bluewin.ch>
2026-02-07 10:07:10 +0530

    Link dolby blobs against v33 libstagefright_foundation
    * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib6178bebceb6eb8eb31ad2bebac4d0ee40b32148
    Signed-off-by: ayano-koji <kiyotaka09876@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -9,9 +9,33 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib/libdlbpreg.so',
+        'vendor/lib/soundfx/libdlbvol.so',
+        'vendor/lib/soundfx/libswdap.so',
+        'vendor/lib/soundfx/libswgamedap.so',
+        'vendor/lib/soundfx/libswspatializer.so',
+        'vendor/lib64/libcodec2_soft_ac4dec.so',
+        'vendor/lib64/libcodec2_soft_ddpdec.so',
+        'vendor/lib64/libdlbdsservice.so',
+        'vendor/lib64/libdlbpreg.so',
+        'vendor/lib64/soundfx/libdlbvol.so',
+        'vendor/lib64/soundfx/libswdap.so',
+        'vendor/lib64/soundfx/libswgamedap.so',
+        'vendor/lib64/soundfx/libswspatializer.so'
+    ): blob_fixup().replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'dolby',
     'motorola',
+    blob_fixups=blob_fixups,
     device_rel_path='vendor/motorola/dolby',
 )
 
