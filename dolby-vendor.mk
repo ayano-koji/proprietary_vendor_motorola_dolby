4187ce192753ed91a11e9553a77dcf954ebfc916
ayano-koji <kiyotaka09876@gmail.com>
2026-02-07 03:00:31 +0000

    Introduce Dolby Atmos
    Change-Id: I9cde98dda4b799e0b03b010ae6cf545bb66a3734
    Signed-off-by: ayano-koji <kiyotaka09876@gmail.com>

--- dolby-vendor.mk
+++ dolby-vendor.mk
@@ -4,3 +4,30 @@
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/dolby
+
+PRODUCT_COPY_FILES += \
+    vendor/motorola/dolby/proprietary/vendor/etc/init/dms-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dms-service.rc \
+    vendor/motorola/dolby/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc \
+    vendor/motorola/dolby/proprietary/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
+PRODUCT_PACKAGES += \
+    libdapparamstorage \
+    libdlbpreg \
+    libdmshal \
+    libspatializerparamstorage \
+    libdlbvol \
+    libswdap \
+    libswgamedap \
+    libswspatializer \
+    vendor.dolby.dms-V1-ndk \
+    vendor.dolby.hardware.dms@2.0 \
+    vendor.dolby.hardware.dms@2.1 \
+    libcodec2_soft_ac4dec \
+    libcodec2_soft_ddpdec \
+    libcodec2_store_dolby \
+    libdeccfg \
+    libdlbdsservice \
+    dms-service.xml \
+    vendor.dolby.media.c2@1.0-service.xml \
+    vendor.dolby.dms.service \
+    vendor.dolby.media.c2@1.0-service
---
+++ motodolby.mk
@@ -0,0 +1,23 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/motorola/dolby/dolby-vendor.mk)
+
+DOLBY_PATH := vendor/motorola/dolby
+
+# Dax
+PRODUCT_COPY_FILES += \
+    $(DOLBY_PATH)/configs/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default-spatializer.xml \
+    $(DOLBY_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(DOLBY_PATH)/configs/dolby/dax-moto_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-moto_1.xml \
+    $(DOLBY_PATH)/configs/dolby/dax-moto_2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-moto_2.xml \
+    $(DOLBY_PATH)/configs/dolby/dax-moto_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-moto_3.xml
+
+# Properties
+TARGET_VENDOR_PROP += $(DOLBY_PATH)/vendor.prop
+
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/configs/vintf/dolby_framework_matrix.xml
